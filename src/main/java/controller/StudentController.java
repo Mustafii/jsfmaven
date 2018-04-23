5d558c9691a8101bcbcdd987d69edfd363065e24
MUSTAFii <mostafizrbn@gmail.com>
2018-04-23 08:54:21 +0600

    mariana Trench

---
+++ src/main/java/controller/StudentController.java
@@ -0,0 +1,21 @@
+package controller;
+
+import data.model.Student;
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean(name="studentcontroller")
+public class StudentController {
+    private Student student=new Student();
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+    public String isNextPage(){
+        return "success";
+    }
+}
